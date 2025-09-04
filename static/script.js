48acf7aac056d0df1adad4e77b2a3081d1806a57
ADITYA SINGH <126189301+tomarjii48@users.noreply.github.com>
2025-09-04 09:48:26 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,21 @@
+async function sendMessage(){
+    let msg=document.getElementById("msg").value;
+    document.getElementById("msg").value='';
+    let resp=await fetch('/webchat',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({text:msg})
+    });
+    let data=await resp.json();
+    let chat=document.getElementById("chatbox");
+    chat.innerHTML+="<div><b>You:</b> "+msg+"</div><div><b>Bot:</b> "+data.reply+"</div>";
+}
 
+async function uploadFile(){
+    let fi=document.getElementById("fileinput");
+    if(!fi.files.length) return;
+    let fd=new FormData(); fd.append('file',fi.files[0]);
+    let res=await fetch('/upload',{method:'POST',body:fd});
+    let data=await res.json();
+    document.getElementById("chatbox").innerHTML+="<div>Uploaded: "+data.filename+"</div>";
+      }
